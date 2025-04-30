d5ed17a9feeeea3eb4c3af72855465a95494be81
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-04-30 17:06:28 +0500

    Updated app.py with faculty dashboard

--- re-flex/app.py
+++ re-flex/app.py
@@ -2,10 +2,13 @@
 
 from pywebio import start_server
 from ui.student_ui import student_dashboard
+from ui.faculty_ui import faculty_dashboard
 
 def main():
     student_id = "S123"  # Later you can implement login and dynamically set this
     student_dashboard(student_id)
+    fac_id="F001"
+    faculty_dashboard(fac_id)
 
 if __name__ == "__main__":
     start_server(main, port=8080)
