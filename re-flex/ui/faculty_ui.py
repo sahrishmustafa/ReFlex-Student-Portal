516adaf7aada51d0a2b5efecff8717a14e83e337
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-04-30 17:07:28 +0500

    Updated faculty_ui.py with removal of cyclic imports

--- re-flex/ui/faculty_ui.py
+++ re-flex/ui/faculty_ui.py
@@ -1,28 +1,19 @@
 from pywebio.output import put_text, put_table, put_buttons, put_markdown, clear
 from pywebio.input import input, file_upload
-from ui.faculty_answer_queries import respond_student_queries
-from ui.faculty_enter_grades     import enter_student_grades
-from ui.faculty_mark_attendance          import mark_attendance
-from ui.faculty_upload_material  import upload_course_materials
-from ui.faculty_upload_assignments import upload_assignments
 
-def faculty_dashboard(faculty_id=None):
-    """
-    Main Faculty Dashboard: displays buttons for all faculty actions.
-    """
+def faculty_dashboard(fac_id=None):
+    from ui import faculty_upload_assignments, faculty_enter_grades, faculty_mark_attendance, faculty_upload_material, faculty_answer_queries
+
     clear()
-    put_markdown(f"# 👨‍🏫 Faculty Dashboard")
+    put_markdown('# 👨‍🏫 Faculty Dashboard')
     put_buttons(
-        ['Upload Assignments',
-         'Enter Student Grades',
-         'Mark Attendance',
-         'Upload Course Materials',
-         'Respond to Student Queries'],
+        ['Upload Assignments', 'Enter Grades', 'Mark Attendance', 
+         'Upload Materials', 'Answer Queries'],
         onclick=[
-            lambda: upload_assignments(),
-            lambda: enter_student_grades(),
-            lambda: mark_attendance(),
-            lambda: upload_course_materials(),
-            lambda: respond_student_queries()
+            lambda: faculty_upload_assignments.upload_assignments(faculty_dashboard),
+            lambda: faculty_enter_grades.enter_student_grades(faculty_dashboard),
+            lambda: faculty_mark_attendance.mark_attendance(faculty_dashboard),
+            lambda: faculty_upload_material.upload_course_materials(faculty_dashboard),
+            lambda: faculty_answer_queries.respond_student_queries(faculty_dashboard)
         ]
     )
