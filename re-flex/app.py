d1a08da8d63a7b5e54b7ab2dfe6c4e5331609f59
Maria Zahid <153418292+merrymystical@users.noreply.github.com>
2025-05-04 11:42:45 +0500

    Updated app.py with all dashboards

--- re-flex/app.py
+++ re-flex/app.py
@@ -1,14 +1,24 @@
-# app.py
-
 from pywebio import start_server
+from pywebio.input  import input, select, TEXT, PASSWORD
+from pywebio.output import put_markdown, clear
+
 from ui.student_ui import student_dashboard
 from ui.faculty_ui import faculty_dashboard
+from ui.admin_ui   import admin_dashboard
 
 def main():
-    student_id = "S123"  # Later you can implement login and dynamically set this
-    student_dashboard(student_id)
-    fac_id="F001"
-    faculty_dashboard(fac_id)
+    clear()
+    put_markdown("# 🔐 ReFlex Login")
+    email = input("Enter your email", type=TEXT, placeholder="you@example.com")
+    # password = input("Password", type=PASSWORD)
+    role  = select("I am logging in as a…", ["Student", "Faculty", "Admin"])
+
+    if role == "Student":
+        student_dashboard(email)
+    elif role == "Faculty":
+        faculty_dashboard(email)
+    else:
+        admin_dashboard(email)
 
 if __name__ == "__main__":
-    start_server(main, port=8080)
+    start_server(main, port=8080, debug=True)
